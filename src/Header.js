8f5f3ae8f125e12e6d5745053aa27ab7fa36ef5a
Kinzamalik <kiinza.malik@gmail.com>
2021-11-07 01:23:00 +0500

    tinder cards

--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import "./Header.css";
+import PersonIcon from '@material-ui/icons/Person';
+import  IconButton  from '@material-ui/core/IconButton';
+import ForumIcon from '@material-ui/icons/Forum';
+
+
 
 function Header() {
     return (
         <div className="header">
-            <h1>header</h1>            
+            <IconButton>
+                    <PersonIcon fontSize="large" className="header__icon"/>         
+            </IconButton> 
+            <img 
+            className="header__logo"
+            src="https://www.logo.wine/a/logo/Tinder_(app)/Tinder_(app)-Flame-Logo.wine.svg"
+            alt=""
+            /> 
+
+            <IconButton>
+                <ForumIcon   fontSize="large" className="header__icon" />
+            </IconButton>
         </div>
     )
 }
